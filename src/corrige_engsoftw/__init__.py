6d2fdb165305a418b652d50e4d3a584b683db9de
kayke <kaykecristofersc@gmail.com>
2026-06-01 22:58:57 -0300

    chore: ajusta exportacoes do pacote

--- src/corrige_engsoftw/__init__.py
+++ src/corrige_engsoftw/__init__.py
@@ -1,5 +1,3 @@
-"""Ferramentas para extrair e comparar protótipos de Engenharia de Software."""
-
 from .artifacts import carregar_artefato
 from .comparator import comparar
 from .extractor import parse_docx
